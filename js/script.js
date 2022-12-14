26637ad26481357bbe7a8ff22a50fb17097ed048
vinogradova8 <112949184+vinogradova8@users.noreply.github.com>
2022-12-14 14:34:18 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+let myAge = 32;
+console.log(typeof myAge);
+console.log(myAge);
+
+let myname = "Vika"
+console.log(myname);
+console.log(typeof myName);
+console.log(typeof myname);
+
+let trueOrFalse = 4 > 10;
+console.log(trueOrFalse);
+
+// Домашка JS (змінні)
+let userClorOfEyes;
+
+let user;
+let userName;
+
+userName = "Вася";
+user = userName;
+console.log(user);
+
+let userAge = 36;
+let userInfo = `Фрілансер ${userAge}`;
+console.log(userInfo);
+
+
